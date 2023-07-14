9d9502b4d39e7f0f737b0f32a8a03b338b72f3e9
douik karim <113842267+jvkar@users.noreply.github.com>
2023-07-14 19:37:08 +0100

    Add files via upload

---
+++ admin/gestionpublication/addmmbr.php
@@ -0,0 +1,88 @@
+<?php
+require "config.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["button1"])) {
+        $titre = $_POST["titre"];
+        $Auteur = $_POST["Auteur"];
+        $nom_pub = $_POST["nom_pub"];
+        $date = $_POST["date"];
+        $type = $_POST["type"];
+        $link =$_POST["link"];
+
+        if (empty($titre) || empty($Auteur) || empty($nom_pub) || empty($date) || empty($type) || empty($link)) {
+            echo "Please enter all required information";
+            header("Refresh:1;URL=gestionenv.php");
+        } else {
+            $request = "SELECT * FROM document WHERE titre=?";
+            $statement = $pdo->prepare($request);
+            $statement->execute(array($titre));
+            $existe_titre = $statement->rowCount();
+
+            if ($existe_titre == 0) {
+                $request = "INSERT INTO document(titre, type, Auteur, nom_pub, date,link) VALUES (?, ?, ?, ?, ?,?)";
+                $statement = $pdo->prepare($request);
+                $statement->execute(array($titre, $type, $Auteur, $nom_pub, $date,$link));
+              
+                header("Refresh:1;URL=gestionenv.php");
+            } else {
+                echo "Title already exists in the database";
+                header("Refresh:1;URL=gestionenv.php");
+            }
+        }
+    } elseif (isset($_POST["button2"])) {
+        $titre = $_POST["titre"];
+        $nom = $_POST["nom"];
+        $prenom = $_POST["prenom"];
+        $date = $_POST["date"];
+        $lieu = $_POST["lieu"];
+
+        if (empty($titre) || empty($nom) || empty($prenom) || empty($date) || empty($lieu)) {
+            echo "Please enter all required information";
+            header("Refresh:3;URL=gestionenv.php");
+        } else {
+            $request = "SELECT * FROM soutenance WHERE titre=?";
+            $statement = $pdo->prepare($request);
+            $statement->execute(array($titre));
+            $existe_titre = $statement->rowCount();
+
+            if ($existe_titre == 0) {
+                $request = "INSERT INTO soutenance(titre, lieu, nom, prenom, date) VALUES (?, ?, ?, ?, ?)";
+                $statement = $pdo->prepare($request);
+                $statement->execute(array($titre, $lieu, $nom, $prenom, $date));
+               
+                header("Refresh:1;URL=gestionenv.php");
+            } else {
+                echo "Title already exists in the database";
+                header("Refresh:1;URL=gestionenv.php");
+            }
+        }
+    } elseif (isset($_POST["button3"])) {
+        $titre = $_POST["titre"];
+        $lieu = $_POST["lieu"];
+        $date = $_POST["date"];
+        $type = $_POST["type"];
+
+        if (empty($titre) || empty($lieu) || empty($date) || empty($type)) {
+            echo "Please enter all required information";
+            header("Refresh:1;URL=gestionenv.php");
+        } else {
+            $request = "SELECT * FROM event WHERE titre=?";
+            $statement = $pdo->prepare($request); 
+            $statement->execute(array($titre));
+            $existe_titre = $statement->rowCount();
+
+            if ($existe_titre == 0) {
+                $request = "INSERT INTO event(titre, lieu, date, type) VALUES (?, ?, ?, ?)";
+                $statement = $pdo->prepare($request);
+                $statement->execute(array($titre, $lieu, $date, $type));
+               
+                header("Refresh:1;URL=gestionenv.php");
+            } else {
+                echo "Title already exists in the database";
+                header("Refresh:2;URL=gestionenv.php");
+            }
+        }
+    }
+}
+?>
